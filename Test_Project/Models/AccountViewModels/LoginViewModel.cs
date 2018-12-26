0ee67fd1c0874ea47979e8f738423f9076fee66f
Yusuf <yusuff.gull@icloud.com>
2018-12-26 21:24:10 +0300

    Çeviriler yapıldı

--- Test_Project/Models/AccountViewModels/LoginViewModel.cs
+++ Test_Project/Models/AccountViewModels/LoginViewModel.cs
@@ -16,7 +16,7 @@ namespace Test_Project.Models.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Beni Hatırla")]
         public bool RememberMe { get; set; }
     }
 }
