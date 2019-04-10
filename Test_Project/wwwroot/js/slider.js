cb9789e0e8277db317907dab1cee517c8b8c7fb5
ebru yazıcı <ebruyazicii1@gmail.com>
2019-04-10 19:55:32 +0300

    Örnek slider yapıldı.ufak düzenlemeleri kaldı.

---
+++ Test_Project/wwwroot/js/slider.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var slider = $('.slider'),
+        list = slider.find('ul.slider_liste'),
+        length = list.find('li').length,
+        width = slider.outerWidth(),
+        totalWidth = width * length,
+        index = 0;
+    list.find('li').width(width).end().width(totalWidth);
+   // alert(length);
+});
